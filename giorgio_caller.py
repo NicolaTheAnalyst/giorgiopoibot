1d064cca7a852308e4e86c7dcfa750cc627f2f93
flyingwonton <66998126+flyingwonton@users.noreply.github.com>
2022-08-04 14:17:51 +0200

    added email functionality.

--- giorgio_caller.py
+++ giorgio_caller.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import yagmail
 import logging
 import sqlite3
 import googleapiclient.discovery
@@ -9,13 +10,28 @@ import googleapiclient.errors
 logging.basicConfig(filename='logcaller.txt', format= "%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger()
 
+sender_email = os.environ.get("sender_mail")
+receiver_email = os.environ.get("receiver_mail")
+yag = yagmail.SMTP(sender_email, oauth2_file='oauth2_creds.json')
+
+def send_email(status, msg):
+    subject = "Giorgio Poi caller log: {status}".format(status = status)
+    body = """Esito: {text}
+    """.format(text = msg)
+    yag.send(to=receiver_email,subject = subject,contents = body)
+    return
+
+
 def table_update(id,title,url):
     logger.info("Connecting to the DB..")
     try:
         sqliteConnection = sqlite3.connect('Songstest.db') #test database
         cursor = sqliteConnection.cursor()
-    except:
+    except Exception as e:
         logger.error("Error while connecting to the DB", exc_info=True)
+        status = "KO"
+        msg = e
+        send_email(status, msg)
 
     logger.info("Querying the DB..")
     try:
@@ -28,11 +44,17 @@ def table_update(id,title,url):
             print(sqlite_insert_query)
             count = cursor.execute(sqlite_insert_query)
             sqliteConnection.commit()
-    except:
-        logger.error("Error while inserting query", exc_info=True)
+    except Exception as e:
+        logger.error("Error while inserting the query", exc_info=True)
+        status = "KO"
+        msg = e
+        send_email(status, msg)
 
     logger.info("Record inserted successfully into SqliteDb_developers table ", cursor.rowcount)
     cursor.close()
+    status = "OK"
+    msg = "OK."
+    send_email(status, msg)
 
 
 def main():
@@ -55,8 +77,11 @@ def main():
             type="video"
         )
         response = request.execute()
-    except:
+    except Exception as e:
         logger.error("Error while calling youtube", exc_info=True)
+        status = "KO"
+        msg = e
+        send_email(status, msg)
 
     logger.info("Cycling on the response")
     try:
@@ -65,8 +90,11 @@ def main():
             id.append(id_number)
             title.append(str(i['snippet']['title']))
             url.append(youtube_prefix + str(i['id']['videoId']))
-    except:
+    except Exception as e:
         logger.error("Error while cycling on the response", exc_info=True)
+        status = "KO"
+        msg = e
+        send_email(status, msg)
 
     table_update(id,title,url)
 
